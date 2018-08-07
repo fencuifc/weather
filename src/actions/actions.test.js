d18c2cd8fe61c303a67172df821cda63858c56cb
Fen Cui <fencui.fc@gmail.com>
2018-08-06 23:20:20 -0400

    update mock for testing

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -4,9 +4,14 @@ import * as menuActions from "./menuActions";
 import * as otherActions from "./other";
 import * as actionTypes from "../constants/ActionTypes";
 // import fetch from "jest-fetch-mock";
+import mockAxios from "jest-mock-axios";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+afterEach(() => {
+	// cleaning up the mess left behind the previous test
+	mockAxios.reset();
+});
 
 describe("Test actions", () => {
 	describe("Menu actions", () => {
@@ -21,62 +26,74 @@ describe("Test actions", () => {
 		});
 	});
 
-	describe("Seach Location actions", () => {
-		//afterEach(() => {});
+	describe("Seach Location action", () => {
 		it("creates RECEIVE_LOCATIONS when FETCH_LOCATIONS has been done", () => {
-			// fetch.mockResponse(
-			// 	JSON.stringify(
-			// 		("data": [
-			// 			{
-			// 				title: "New York",
-			// 				location_type: "City",
-			// 				woeid: 2459115,
-			// 				latt_long: "40.71455,-74.007118"
-			// 			}
-			// 		])
-			// 	)
-			// );
-			const expectedActions = [
-				{ type: actionTypes.FETCH_LOCATIONS, searchTerm: "new york" },
+			let catchFn = jest.fn(),
+				thenFn = jest.fn();
+
+			const mockData = [
 				{
-					type: actionTypes.RECEIVE_LOCATIONS,
-					locations: [
-						{
-							title: "New York",
-							location_type: "City",
-							woeid: 2459115,
-							latt_long: "40.71455,-74.007118"
-						}
-					]
+					title: "New York TEST",
+					location_type: "City",
+					woeid: 2459115,
+					latt_long: "40.71455,-74.007118"
 				}
 			];
+
+			const expectedActions = [
+				{ type: actionTypes.FETCH_LOCATIONS, searchTerm: "new york" },
+				{ type: actionTypes.RECEIVE_LOCATIONS, locations: mockData }
+			];
+
 			const store = mockStore({});
-			return store
+
+			store
 				.dispatch(otherActions.fetchLoctions("new york"))
-				.then(() => {
-					// return of async actions
-					expect(store.getActions()).toEqual(expectedActions);
-				});
+				.then(thenFn)
+				.catch(catchFn);
+
+			expect(mockAxios.get).toHaveBeenCalledWith(
+				"/api/location/search/?query=new york"
+			);
+
+			mockAxios.mockResponse({ data: mockData });
+			expect(store.getActions()).toEqual(expectedActions);
 		});
 	});
 
 	//2347591, New York
 
-// describe("select location actions", () => {
-// 	//afterEach(() => {});
-// 	it("creates action when SELECT_LOCATION has been done", () => {
-// 		fetch.mockResponse(JSON.stringify({"data": "locationResponse"}));
-// 		const expectedActions = [
-// 			{ type: types.SELECT_LOCATION, woeid: 2459115 },
-// 			{ type: types.FETCH_LOCATIONS, searchTerm: "New York" },
-// 			{ type: types.RECEIVE_LOCATIONS,locations: []},
-// 			{ type: types.RECEIVE_WEATHER,location:"locationResponse"}
-// 		];
-// 		const store = mockStore({});
-// 		return store.dispatch(actions.selectLocation(2459115,"New York")).then(() => {
-// 			// return of async actions
-// 			expect(store.getActions()).toEqual(expectedActions);
-// 		});
-// 	});
-// });
+	describe("Select location action", () => {
+		it("creates action when SELECT_LOCATION has been done", () => {
+			let catchFn = jest.fn(),
+				thenFn = jest.fn();
+
+			const mockData = [
+				{
+					title: "New York TEST",
+					location_type: "City",
+					woeid: 2459115,
+					latt_long: "40.71455,-74.007118"
+				}
+			];
+
+			const expectedActions = [
+				{ type: actionTypes.SELECT_LOCATION, woeid: 2459115 },
+				{ type: actionTypes.FETCH_LOCATIONS, searchTerm: "New York" },
+				{ type: actionTypes.RECEIVE_LOCATIONS, locations: [] },
+				{ type: actionTypes.RECEIVE_WEATHER, location: mockData }
+			];
+			const store = mockStore({});
+			store
+				.dispatch(otherActions.selectLocation(2459115, "New York"))
+				.then(thenFn)
+				.catch(catchFn);
+
+			expect(mockAxios.get).toHaveBeenCalledWith(
+				"/api/location/2459115/"
+			);
+			mockAxios.mockResponse({ data: mockData });
+			expect(store.getActions()).toEqual(expectedActions);
+		});
+	});
 });
